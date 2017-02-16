a5754d64f18b11fc93f46a13f94923e94665408c
AlexPnv <a.panferov10@gmail.com>
2017-02-16 22:57:11 +0300

    Add files via upload

---
+++ assets/js/google-map.js
@@ -0,0 +1,120 @@
+google.maps.event.addDomListener(window, 'load', init);
+
+var map;
+
+function init() {
+	var mapOptions = {
+		center: new google.maps.LatLng(52.086416, 23.698108),
+		zoom: 17,
+		zoomControl: true,
+		zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.SMALL,
+		},
+		disableDoubleClickZoom: true,
+		mapTypeControl: true,
+		mapTypeControlOptions: {
+			style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+		},
+		scaleControl: true,
+		scrollwheel: true,
+		streetViewControl: true,
+		draggable : true,
+		overviewMapControl: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: [
+		{
+			featureType: "landscape",
+			stylers: [
+			{ saturation: -100 },
+			{ lightness: 65 },
+			{ visibility: "on" }
+			]
+		},{
+			featureType: "poi",
+			stylers: [
+			{ saturation: -100 },
+			{ lightness: 51 },
+			{ visibility: "simplified" }
+			]
+		},{
+			featureType: "road.highway",
+			stylers: [
+			{ saturation: -100 },
+			{ visibility: "simplified" }
+			]
+		},{
+			featureType: "road.arterial",
+			stylers: [
+			{ saturation: -100 },
+			{ lightness: 30 },
+			{ visibility: "on" }
+			]
+		},{
+			featureType: "road.local",
+			stylers: [
+			{ saturation: -100 },
+			{ lightness: 40 },
+			{ visibility: "on" }
+			]
+		},{
+			featureType: "transit",
+			stylers: [
+			{ saturation: -100 },
+			{ visibility: "simplified" }
+			]
+		},{
+			featureType: "administrative.province",
+			stylers: [
+			{ visibility: "off" }
+			]
+	/** /
+		},{
+			featureType: "administrative.locality",
+			stylers: [
+				{ visibility: "off" }
+			]
+		},{
+			featureType: "administrative.neighborhood",
+			stylers: [
+				{ visibility: "on" }
+			]
+			/**/
+		},{
+			featureType: "water",
+			elementType: "labels",
+			stylers: [
+			{ visibility: "on" },
+			{ lightness: -25 },
+			{ saturation: -100 }
+			]
+		},{
+			featureType: "water",
+			elementType: "geometry",
+			stylers: [
+			{ hue: "#ffff00" },
+			{ lightness: -25 },
+			{ saturation: -97 }
+			]
+		}
+		],
+		
+	}
+
+	
+}
+
+var myLatlng = new google.maps.LatLng(52.086416, 23.698108);
+var mapOptions = {
+  zoom: 18,
+  center: myLatlng
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+var marker = new google.maps.Marker({
+    position: myLatlng,
+    title:"Мы тут:)",
+    animation: google.maps.Animation.BOUNCE,
+});
+
+// To add the marker to the map, call setMap();
+marker.setMap(map);
